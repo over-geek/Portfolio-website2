5e1cff470065063b743ca80be0a340347be62117
kenagy100@gmail.com <kenagy100@gmail.com>
2023-05-15 22:21:51 +0000

    Fix linter errors

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const menuBtn = document.getElementById("menu-btn-open");
-const overlay = document.getElementById("overlay");
-const menu = document.getElementById("mobile-menu");
+const menuBtn = document.getElementById('menu-btn-open');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
 
 function navToggle() {
-  menuBtn.classList.toggle("open");
-  overlay.classList.toggle("overlay-show");
-  document.body.classList.toggle("stop-scrolling");
-  menu.classList.toggle("show-menu");
+  menuBtn.classList.toggle('open');
+  overlay.classList.toggle('overlay-show');
+  document.body.classList.toggle('stop-scrolling');
+  menu.classList.toggle('show-menu');
 }
 
-menuBtn.addEventListener("click", navToggle);
+menuBtn.addEventListener('click', navToggle);
 
 document.documentElement.addEventListener('click', (e) => {
   if (e.target.matches('.menu-item')) {
