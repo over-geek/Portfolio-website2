f5d66b11e9776cb43b44023a5b90fb4fb3fd8d86
kenagy100@gmail.com <kenagy100@gmail.com>
2023-05-15 22:01:40 +0000

    Add stop scrolling event listener to mobile menu

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function navToggle() {
 }
 
 menuBtn.addEventListener("click", navToggle);
+
+document.documentElement.addEventListener('click', (e) => {
+  if (e.target.matches('.menu-item')) {
+    menu.classList.remove('show-menu');
+    menuBtn.classList.remove('open');
+    overlay.classList.remove('overlay-show');
+    document.body.classList.remove('stop-scrolling');
+  }
+});
