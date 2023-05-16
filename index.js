06071fb7c2586e986c0e2c8c2af11a6d71883971
kenagy100@gmail.com <kenagy100@gmail.com>
2023-05-16 15:28:18 +0000

    Add modal data to js code

--- index.js
+++ index.js
@@ -19,3 +19,44 @@ document.documentElement.addEventListener('click', (e) => {
     document.body.classList.remove('stop-scrolling');
   }
 });
+
+// Popup window
+const workData = [
+  {
+    image: 'asset/snapshot1.svg',
+    name: 'Tonic',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    skills: ['html', 'css', 'javascript'],
+    source: 'https://github.com/over-geek/Portfolio-website2',
+    livelink: 'https://over-geek.github.io/overgeek.github.io/',
+    client_info: ['canopy', 'Back End Dev', '2015']
+  },
+  {
+    image: 'asset/snapshot2.svg',
+    name: 'Multi-Post Stories',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    skills: ['html', 'css', 'javascript'],
+    source: 'https://github.com/over-geek/Portfolio-website2',
+    livelink: 'https://over-geek.github.io/overgeek.github.io/',
+    client_info: ['canopy', 'Back End Dev', '2015']
+  },
+  {
+    image: 'asset/snapshot3.svg',
+    name: 'Tonic',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    skills: ['html', 'css', 'javascript'],
+    source: 'https://github.com/over-geek/Portfolio-website2',
+    livelink: 'https://over-geek.github.io/overgeek.github.io/',
+    client_info: ['canopy', 'Back End Dev', '2015']
+  },
+  {
+    image: 'asset/snapshot4.svg',
+    name: 'Multi-Post Stories',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    skills: ['html', 'css', 'javascript'],
+    source: 'https://github.com/over-geek/Portfolio-website2',
+    livelink: 'https://over-geek.github.io/overgeek.github.io/',
+    client_info: ['canopy', 'Back End Dev', '2015']
+  },
+
+]
