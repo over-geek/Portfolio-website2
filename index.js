71ca9dc8df1f5033568d6a7d3a7c543c45a541c3
kenagy100@gmail.com <kenagy100@gmail.com>
2023-05-18 16:10:37 +0000

    Add function to get values from storage

--- index.js
+++ index.js
@@ -183,3 +183,30 @@ contactForm.addEventListener('submit', (e) => {
     errorMessage.style.display = 'block';
   }
 });
+
+// Local Storage
+function storage(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      e.code === 22
+      || e.code === 1014
+      || e.name === 'QuotaExceededError'
+      || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      && (storage && storage.length !== 0);
+  }
+}
+
+// Getting values from storage
+function setInputsData() {
+  const inputsDataObj = JSON.parse(localStorage.getItem('inputsData'));
+  document.getElementById('fullname').value = inputsDataObj.name;
+  document.getElementById('mail').value = inputsDataObj.email;
+  document.getElementById('message').value = inputsDataObj.message;
+}
